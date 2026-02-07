bd82a488a039a3139521393551a1cf6cf2f21096
Fire <jamesli28000@gmail.com>
2026-02-07 16:29:52 -0500

    yeah

--- app/page.tsx
+++ app/page.tsx
@@ -121,7 +121,7 @@ export default function Home() {
             <motion.div
               key="favicon-loader"
               initial={{ opacity: 1}}
-              animate={{ opacity: 1, ease: "easeOut"}}
+              animate={{ opacity: 1}}
               exit={{ opacity: 0}}
               transition={{ duration:1}}
             >
